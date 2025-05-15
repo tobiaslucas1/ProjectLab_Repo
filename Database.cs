a80a3c0f61cadfe7b33922086993283425ffd284
tobiaslucas1 <r1023350@student.thomasmore.be>
2025-05-15 14:20:16 +0200

    upgrades

---
+++ Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test
+{
+    public static class Database
+    {
+        public static List<Driver> Drivers { get; set; } = new List<Driver>();
+    }
+}
