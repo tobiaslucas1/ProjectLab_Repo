9abd54f5a425a9aeacf4f6932bf1202666dd1b11
tobiaslucas1 <r1023350@student.thomasmore.be>
2025-05-15 14:04:21 +0200

    RegisterDriver Updated Working and linked to Loginpage

---
+++ Driver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Test
+{
+    public class Driver
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string CarModel { get; set; }
+        public string Plate { get; set; }
+        public string Color { get; set; }
+        public bool AllowsSmoking { get; set; }
+        public bool PlaysMusic { get; set; }
+    }
+}
