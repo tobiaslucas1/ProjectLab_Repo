189843f443d2271f218482ec4bf6ada71a242c62
tobiaslucas1 <r1023350@student.thomasmore.be>
2025-05-30 22:05:25 +0200

    finale

--- EditRoadTripPage.xaml.cs
+++ EditRoadTripPage.xaml.cs
@@ -24,21 +24,59 @@ namespace Test
         {
             InitializeComponent();
             selectedRoadTrip = roadTrip;
+
+            StartCityComboBox.ItemsSource = BelgiumCities.Cities;
+            EndCityComboBox.ItemsSource = BelgiumCities.Cities;
+
             LoadRoadTripDetails();
         }
 
+
         private void LoadRoadTripDetails()
         {
-            StartCityTextBox.Text = selectedRoadTrip.StartCity;
-            EndCityTextBox.Text = selectedRoadTrip.EndCity;
+            StartCityComboBox.SelectedItem = selectedRoadTrip.StartCity;
+            EndCityComboBox.SelectedItem = selectedRoadTrip.EndCity;
             DepartureDatePicker.SelectedDate = selectedRoadTrip.DepartureDate;
             DepartureTimeTextBox.Text = selectedRoadTrip.DepartureTime;
         }
 
+
+        public static class BelgiumCities
+        {
+            public static List<string> Cities = new List<string>
+    {
+        "Aalst", "Andenne", "Antwerpen", "Arlon", "Asse",
+        "Aarschot", "Bastogne", "Beringen", "Bilzen", "Binche",
+        "Boom", "Bouillon", "Braine-le-Château", "Braine-l’Alleud", "Brasschaat",
+        "Brugge", "Brussel", "Châtelet", "Charleroi", "Chaudfontaine",
+        "Ciney", "Courcelles", "Dinant", "Diest", "Dilbeek",
+        "Dendermonde", "Eupen", "Eghezée", "Floreffe", "Fosses-la-Ville",
+        "Frameries", "Genappe", "Genk", "Geel", "Gembloux",
+        "Geraardsbergen", "Gent", "Grimbergen", "Habay", "Halle",
+        "Hannut", "Hasselt", "Havelange", "Herentals", "Herstal",
+        "Heusden-Zolder", "Houffalize", "Houthalen-Helchteren", "Huy", "Ieper",
+        "Jodoigne", "Knokke-Heist", "Kortrijk", "La Louvière", "Leuven",
+        "Libramont-Chevigny", "Liège", "Lier", "Lommel", "Lokeren",
+        "Luik", "Manage", "Maasmechelen", "Marche-en-Famenne", "Mechelen",
+        "Menen", "Mons", "Mortsel", "Mouscron", "Namur",
+        "Namen", "Neufchâteau", "Ninove", "Nivelles", "Oostende",
+        "Ottignies-Louvain-la-Neuve", "Oudenaarde", "Profondeville", "Rixensart", "Roeselare",
+        "Saint-Hubert", "Saint-Nicolas", "Sambreville", "Seraing", "Sint-Niklaas",
+        "Sint-Truiden", "Tielt", "Tienen", "Torhout", "Tongeren",
+        "Tournai", "Turnhout", "Tubize", "Vilvoorde", "Visé",
+        "Virton", "Wavre", "Waregem", "Waterloo", "Wetteren",
+        "Willebroek", "Zaventem", "Zottegem"
+    };
+        }
+
+
+
         private void SaveChanges_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(StartCityTextBox.Text) ||
-                string.IsNullOrWhiteSpace(EndCityTextBox.Text) ||
+            string startCity = StartCityComboBox.SelectedItem as string;
+            string endCity = EndCityComboBox.SelectedItem as string;
+
+            if (string.IsNullOrEmpty(startCity) || string.IsNullOrEmpty(endCity) ||
                 !DepartureDatePicker.SelectedDate.HasValue ||
                 string.IsNullOrWhiteSpace(DepartureTimeTextBox.Text))
             {
@@ -46,14 +84,15 @@ namespace Test
                 return;
             }
 
-            selectedRoadTrip.StartCity = StartCityTextBox.Text;
-            selectedRoadTrip.EndCity = EndCityTextBox.Text;
+            selectedRoadTrip.StartCity = startCity;
+            selectedRoadTrip.EndCity = endCity;
             selectedRoadTrip.DepartureDate = DepartureDatePicker.SelectedDate.Value;
             selectedRoadTrip.DepartureTime = DepartureTimeTextBox.Text;
 
             MessageBox.Show("Roadtrip succesvol aangepast!");
             NavigationService?.Navigate(new Admin_RoadTrips());
         }
+
 
         private void Home_Click(object sender, RoutedEventArgs e)
         {
